6b2ef11a4be00daf34fb68e2ec3989b582863f7f
sdcosta8 <sdcosta@caltech.edu>
2018-04-11 10:34:23 -0700

    Reads in the mu file and stores the users in a hashmap

--- read_data.cpp
+++ read_data.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <vector>
+#define SIZE  10000
 using namespace std;
 
 int main()
 {
 	ifstream inFile;
-    inFile.open("../mu/all.dta");
+	inFile.open("all.dta");
 	if (!inFile) {
 		cout << "File not opened." << endl;
 		exit(1);
 	}
-
+	
 	// The matrix of all of the ratings
-	int rating_matrix[10][4];
+	int rating_matrix[SIZE][4];
 
 
-    for (int i=0; i<10; i++) {
-		for (int j=0; j<4; j++) {
+	for (int i = 0; i<SIZE; i++) {
+		for (int j = 0; j<4; j++) {
 			inFile >> rating_matrix[i][j];
 		}
-    }
-	inFile.close();
+	}
 
-	for (int i=0; i<10; i++) {
-		for (int j=0; j<4; j++) {
-			cout << rating_matrix[i][j] << " ";
+	map<int, vector<int>> users;
+	for (int i = 0; i < SIZE; i++)
+	{
+		int user = rating_matrix[i][0];
+
+		if (users.find(user) == users.end())
+		{
+			vector<int> movie;
+			movie.push_back(rating_matrix[i][3]);
+			users[user] = movie;
+		}
+		else
+		{
+			vector<int> movies;
+			movies = users[user];
+			movies.push_back(rating_matrix[i][3]);
+			users[user] = movies;
+
+		}
+	}
+
+
+	for (map<int, vector<int> >::iterator ii = users.begin(); ii != users.end(); ++ii) {
+		cout << (*ii).first << ": ";
+		vector <int> inVect = (*ii).second;
+		for (unsigned j = 0; j<inVect.size(); j++) {
+			cout << inVect[j] << " ";
 		}
 		cout << endl;
-    }
+	}
+	cout << "Map size: " << users.size() << endl;
 	return 0;
 }
