8d34a77a3110076678cf872b38c401ad0643db98
sdcosta8 <sdcosta@caltech.edu>
2018-04-12 02:11:48 -0700

    creates two hashmaps one for average users and one for average movies
    There is a function given a user and movie. The hashmap is queried for both and the aggregate movie and user rating.

--- read_data.cpp
+++ read_data.cpp
@@ -2,8 +2,57 @@
 #include <fstream>
 #include <map>
 #include <vector>
-#define SIZE  10000
+#define USER_SIZE  32000
+#define MOVIE_SIZE  32000
 using namespace std;
+
+map<int, double> users;
+map<int, double> movies;
+
+void populate_hashmap(int size, int rating_matrix[][4], map<int, double> &temp_map, char type)
+{
+	if (type = 'u')
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int user = rating_matrix[i][0];
+			if (temp_map.find(user) == temp_map.end())
+			{
+
+				temp_map[user] = (double)rating_matrix[i][3];
+			}
+			else
+			{
+				double temp = temp_map.find(user)->second;
+				temp_map[user] = ((double)rating_matrix[i][3] + temp) / 2.0;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int user = rating_matrix[i][3];
+
+			if (temp_map.find(user) == temp_map.end())
+			{
+
+				temp_map[user] = (double)rating_matrix[i][0];
+			}
+			else
+			{
+				double temp = temp_map.find(user)->second;
+				temp_map[user] = ((double)rating_matrix[i][0] + temp) / 2.0;
+			}
+		}
+	}
+
+}
+
+double baseline_pred(int user, int movie)
+{
+	return (users[user] + movies[movie]) / 2.0;
+}
 
 int main()
 {
@@ -13,33 +62,42 @@ int main()
 		cout << "File not opened." << endl;
 		exit(1);
 	}
-	
+
 	// The matrix of all of the ratings
-	int rating_matrix[SIZE][4];
+	int rating_matrix_users[USER_SIZE][4];
+	int rating_matrix_movies[MOVIE_SIZE][4];
 
 
-	for (int i = 0; i<SIZE; i++) {
+	for (int i = 0; i < USER_SIZE; i++) {
 		for (int j = 0; j<4; j++) {
-			inFile >> rating_matrix[i][j];
+			inFile >> rating_matrix_users[i][j];
 		}
 	}
 
-	map<int, double> users;
-	for (int i = 0; i < SIZE; i++)
-	{
-		int user = rating_matrix[i][0];
+	populate_hashmap(USER_SIZE, rating_matrix_users, users, 'u');
 
-		if (users.find(user) == users.end())
-		{
-			
-			users[user] = (double) rating_matrix[i][3];
-		}
-		else
-		{
-			double temp = users[user];
-			users[user] = ((double)rating_matrix[i][3] + temp) / 2.0;
+
+	for (int i = 0; i<MOVIE_SIZE; i++) {
+		for (int j = 0; j<4; j++) {
+			inFile >> rating_matrix_movies[i][j];
 		}
 	}
+	populate_hashmap(MOVIE_SIZE, rating_matrix_movies, movies,'m');
+
+
 	
-	return 0;
+	cout << "Map size: " << users.size() << endl;
+	cout << " Map movie size: " << movies.size() << endl;
+	/*
+	for (auto elem : movies)
+	{
+		std::cout << elem.first << " " << elem.second << "\n";
+	}
+	for (auto elem : users)
+	{
+		std::cout << elem.first << " " << elem.second << "\n";
+	}
+	*/
+
+ 	return 0;
 }
