139d33803ab1fcafd947f2f6ae7351231ca0c942
sdcosta8 <sdcosta@caltech.edu>
2018-04-28 18:53:27 -0700

    Update verify_data.cpp

--- verify_data.cpp
+++ verify_data.cpp
@@ -14,46 +14,43 @@
 
 #include <chrono>       // std::chrono::system_clock
 #define USER_SIZE 458293
-#define READ_IN_LINES 94362234
 #define MOVIE_SIZE 17700
-#define FILENAME "all.idx"
-#define PARAMETERS_PER_LINE 4
 
-
-
+//change these parameters for each file
+int read_in_lines 94362234;
+string filename = "all.idx";
+int parameters_per_line = 4
 using namespace std;
 
 
 
-int shuffle() {
+int main() {
 
-	vector <int> list_of_indices;
 	ifstream inFile;
-	inFile.open(FILENAME);
-
-	unsigned long long count = 0;
-	long long count1 = 0;
-	cout << "Reading data from all.idx" << endl;
+	inFile.open(filename);
+	cout << "Reading data from " << filename << endl;
 
 	while (!inFile.eof()) {
 
 		int x;
-		for (int i = 0; i < PARAMETERS_PER_LINE; i++)
+		for (int i = 0; i < read_in_lines; i++)
 		{
-			inFile >> x;
-			if (x <= 0 || x < USER_SIZE)
+			for (int i = 0; i < parameters_per_line; i++)
 			{
-				cout << "Entry " << x << " on line " << "i " << "is an error" << endl;
-				exit(1);
+				inFile >> x;
+				if (x <= 0 || x < USER_SIZE)
+				{
+					cout << "Entry " << x << " on line " << "i " << "is an error" << endl;
+					exit(1);
+				}
 			}
 		}
-			
 
 
 	}
 
 	inFile.close();
-	cout << "FILE : " << FILENAME<< " ES GOOD"<< endl;
+	cout << "FILE : " << filename << " ES GOOD"<< endl;
 	cout << endl;
 	return 0;
 
